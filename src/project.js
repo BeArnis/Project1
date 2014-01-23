6f5ff31b20e1dbb356f9b0b59dabcd82f7e0c8c1
BeArnis <berzins555@gmail.com>
2014-01-23 21:01:50 +0200

    method exists_link added

--- src/project.js
+++ src/project.js
@@ -51,7 +51,8 @@ function rep_init() {
         exists_instance: function (instance_name) {
             return instance_name in this.instances;
         },
-        add_link: function () {}
+        add_link: function () {},
+        exists_link: function () {}
 
     };
     return repository;
